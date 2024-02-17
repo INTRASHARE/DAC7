47281980342201c2c0adb2f849100cf4981b87ea
sandeep <sandeep-chauhan-self>
2024-02-18 03:06:42 +0530

    password encryption done

--- server/TestingDataset.js
+++ server/TestingDataset.js
@@ -2,12 +2,13 @@ import faker from "faker";
 import fs from "fs";
 
 const generateUsers = (count) => {
+
   const users = [];
 
   for (let i = 1; i <= count; i++) {
     const user = {
       eId: i.toString(),
-      password: i.toString(),
+      password: "$2b$10$IPKfd5sD2qT2.HrfqC0EPuchQ4aJboIPYCVEro54KbGYl.TGLgZMm",
       email: faker.internet.email(),
       name: faker.name.findName(),
       profilePicture: "/default_avatar.png",
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,44 +1,76 @@
 import getPrismaInstance from "../utils/PrismaClient.js";
+import bcrypt from "bcrypt";
 
+function hashPassword(password, saltRounds) {
+  return new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(hash);
+          }
+      });
+  });
+}
+
+async function checkPassword(plainPassword, hashedPassword) {
+  try {
+      const match = await bcrypt.compare(plainPassword, hashedPassword);
+      return match;
+  } catch (error) {
+      console.error(error);
+      return false; // Return false in case of an error
+  }
+}
+ 
 export const checkUser = async (request, response, next) => {
-    try {
-        const { eId, password } = request.body;
-        console.log("request bodu at checkuser server", request.body);
-        if (!eId) {
-            return response.json({ msg: "Employee ID is required", status: false });
-        }
-        const prisma = getPrismaInstance();
-        const user = await prisma.user.findFirst({ where: { eId, password } });
-
-        console.log("user in server ", user.eId);
-        if (!user) {
-            return response.json({ msg: "User not found", status: false });
-        } else if(!user.isActive){
-          return response.json({ msg: "User not found", status: false });
-
-        } else {
-            return response.json({ msg: "User found", status: true, data: user });
-        }
-    } catch (error) {
-        next(error);
+  try {
+    var { eId, password } = request.body;
+    if (!eId) {
+      return response.json({ msg: "Employee ID is required", status: false });
     }
+    const prisma = getPrismaInstance();
+    const saltRounds = 10;
+    const bcryptPassword = await hashPassword(password, saltRounds);
+
+    const user = await prisma.user.findFirst({ where: { eId } });
+
+    console.log("user in server ", user);
+    if (!user) {
+      return response.json({ msg: "User not found", status: false });
+    } else if (!user.isActive) {
+      return response.json({ msg: "User not found", status: false });
+    } else {
+
+      checkPassword(password, bcryptPassword)
+    .then(match => {
+        if (match) {
+           return response.json({ msg: "User found", status: true, data: user });
+        } else {
+          return response.json({ msg: "Incorrect Passowrd", status: false });
+        }
+    });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const onBoardUser = async (request, response, next) => {
-    try {
-        const { eId, name, about = "Available", image:profilePicture } = request.body;
-        const prisma = getPrismaInstance();
+  try {
+    const { eId, name, about = "Available", image: profilePicture } = request.body;
+    const prisma = getPrismaInstance();
 
-        console.log(request.body);
-        await prisma.user.update({
-            where: { eId: eId },
-            data: { name, about, profilePicture, onBoarding: 1 }
-        });
+    console.log(request.body);
+    await prisma.user.update({
+      where: { eId: eId },
+      data: { name, about, profilePicture, onBoarding: 1 }
+    });
 
-        return response.json({ msg: "Success", status: true });
-    } catch (error) {
-        next(error);
-    }
+    return response.json({ msg: "Success", status: true });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getAllUsers = async (req, res, next) => {
